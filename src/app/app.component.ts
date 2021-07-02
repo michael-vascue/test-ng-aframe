871f48535e064328e78961a9ac7136d8a7ca48cf
Mic <micftk@gmail.com>
2021-07-02 16:35:04 +0800

    feat: added control of aframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-aframe';
+  @ViewChild('yellowBox') yellowBox!: ElementRef;
+  
+  constructor() { }
+
+  playSound() {
+    console.log("play")
+    this.yellowBox.nativeElement.components.sound.playSound();
+  }
+
+  stopSound() {
+    console.log("stop")
+    this.yellowBox.nativeElement.components.sound.stopSound();
+  }
 }
